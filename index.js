e2f83e5cf3b685d8ffe6f4f3a17d993a1c00c3be
Nayahto <nayahto.147@gmail.com>
2022-06-04 11:40:25 -0300

    novo comite

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { urlencoded } = require("body-parser")
 const express = require("express")
+const { redirect } = require("express/lib/response")
 const app = express()
 const path = require("path")
 const port = 3000
@@ -9,6 +10,17 @@ app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded())
 
+const form_dado = 
+[{
+    id: 1,
+    imagem: "imagem",
+    lancamento: "lancamento",
+    nome: "nome",
+    descricao: "descricao"
+
+}]
+
+let film = undefined
 
 app.get("/", (req, res) => {
     res.send("ola mundo")
@@ -17,5 +29,13 @@ app.get("/", (req, res) => {
 app.get("/index", (req, res) => {
     res.render("index")
 })
+
+app.post("/create", (req, res) => {
+    const film = req.body
+    film.id = form_dado.length + 1
+    form_dado.push(film)
+   res.redirect('/#prc')
+})
+
 
 app.listen(port)
