793bf8ce2b91898bf5c0b9d02658ab79834a3ed0
Nayahto <nayahto.147@gmail.com>
2022-06-07 23:12:55 -0300

    comite

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # catalog
 third project to blue`s second module 
+
+
+
+*um projeto simples, mas que coloquei todo meu empenho
+aqui você vera uma pagina de filmes onde você e o administrador
+cadastrar filmes e apagar sera a sua função
+
+*a pagina chamada detalhes vai te levar ate os detalhes e o catalogo completo de filmes
+
+*a pagina “cadastro” que não chega a ser uma pagina e mais um redirecionar vai te levar ao lugar onde os filmes são de fato cadastrado
+tentei usar uma paleta de cores bem simples abusando dos tons neutros como cinza, preto e branco e isso por todas as outras paginas
+
+*simplicidade e a palavra que define esse projeto tentei ser o mais simples e intuitivo possível adicionando “labels” logo acima dos inputs
+um “footer” que imagino ser padrão, com links para possíveis contatos
+dividi o máximo do código que eu consegui com “sections”, “articles”, “divs”, “ids”, etc.
+
+*e o mesmo padrão segue pelo CSS onde eu separei o core dos outros tipos de eventos por comentário, por exemplo: hovers e @media estão separados do resto do código
+
+*JavaScript comentado para a fácil compreensão
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+//constantes
 const { urlencoded } = require("body-parser")
 const express = require("express")
 const { redirect } = require("express/lib/response")
 const app = express()
 const path = require("path")
 const port = 3000
-
-let teste = undefined
+// configuracoes ejs e express
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded())
-
+// objeto
 const form_dado = [{
 id: 1,
 imagem: "https://br.web.img2.acsta.net/medias/nmedia/18/89/56/94/20055685.jpg",
@@ -17,32 +17,23 @@ lancamento: "16/01/1998",
 nome: "titanic",
 descricao: "É uma história de ficção do naufrágio real do RMS Titanic, estrelando Leonardo DiCaprio como Jack Dawson, e Kate Winslet como Rose DeWitt Bukater, membros de diferentes classes sociais que se apaixonam durante a fatídica viagem inaugural no navio saindo de Southampton para Nova York em 15 de abril de 1912."
 }]
-
+// roteamento
 app.get("/catalogo", (req, res) => {
 res.render("catalogo", {form_dado})
 })
-
-app.get("/detalhes", (req, res) => {
-  res.render("detalhes",{form_dado })
-  })
 
 app.post("/see", (req, res) => {
 const nov = req.body
 nov.id = form_dado.length + 1
 form_dado.push(nov)
 })
-    
+
 app.get("/delete/:id", (req, res) => {
     const id = +req.params.id - 1; 
     delete form_dado[id];
     res.redirect("/");
   });
 
-  app.get("/detalhes/:id", (req, res) => {
-    const id_2 = +req.params.id;
-    teste = form_dado.find((batata) => batata.id == id_2);
-    res.redirect("detalhes");
-  })
 app.get("/", (req, res) => {
 res.render("index", {form_dado})
 })
@@ -53,7 +44,5 @@ film.id = form_dado.length + 1
 form_dado.push(film)
 res.redirect('/')
 })
-
-app
-
+//server on
 app.listen(port)
